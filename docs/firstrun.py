89792990dd63ba6562d149755ce4fdc9f3d9af9e
Eden Hasson <edenhasson@gmail.com>
2023-05-12 17:15:38 +0300

    1.7

--- docs/firstrun.py
+++ docs/firstrun.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000)
 
